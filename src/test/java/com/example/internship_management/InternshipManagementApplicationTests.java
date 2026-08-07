8a007e3f0286d7169bc0a608caf36b593d59d4d8
HuyDung <nguyenhdung2006@gmail.com>
2026-08-07 11:18:26 +0700

    phase-01: initialize project baseline

---
+++ src/test/java/com/example/internship_management/InternshipManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.internship_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class InternshipManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
